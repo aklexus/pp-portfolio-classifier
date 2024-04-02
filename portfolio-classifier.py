87f15727e5c00e4d1c23954f34f7247eacc66a52
alex <alexander.pumple@gmail.com>
2024-04-02 14:15:41 +0200

    move constants and taxonomies to seperate files

--- portfolio-classifier.py
+++ portfolio-classifier.py
@@ -15,85 +15,11 @@ from bs4 import BeautifulSoup
 from jinja2 import Environment, BaseLoader
 from jsonpath_ng import parse
 
+from utils.CONSTANTS import COLORS
+from utils.taxonomies import taxonomies
+
 requests_cache.install_cache(expire_after=60 * 60 * 24)  # cache downloaded files for a day
 requests_cache.remove_expired_responses()
-
-COLORS = ["#EFDECD", "#CD9575", "#FDD9B5", "#78DBE2", "#87A96B", "#FFA474", "#FAE7B5", "#9F8170", "#FD7C6E", "#000000",
-          "#ACE5EE", "#1F75FE", "#A2A2D0", "#6699CC", "#0D98BA", "#7366BD", "#DE5D83", "#CB4154", "#B4674D", "#FF7F49",
-          "#EA7E5D", "#B0B7C6", "#FFFF99", "#1CD3A2", "#FFAACC", "#DD4492", "#1DACD6", "#BC5D58", "#DD9475", "#9ACEEB",
-          "#FFBCD9", "#FDDB6D", "#2B6CC4", "#EFCDB8", "#6E5160", "#CEFF1D", "#71BC78", "#6DAE81", "#C364C5", "#CC6666",
-          "#E7C697", "#FCD975", "#A8E4A0", "#95918C", "#1CAC78", "#1164B4", "#F0E891", "#FF1DCE", "#B2EC5D", "#5D76CB",
-          "#CA3767", "#3BB08F", "#FEFE22", "#FCB4D5", "#FFF44F", "#FFBD88", "#F664AF", "#AAF0D1", "#CD4A4C", "#EDD19C",
-          "#979AAA", "#FF8243", "#C8385A", "#EF98AA", "#FDBCB4", "#1A4876", "#30BA8F", "#C54B8C", "#1974D2", "#FFA343",
-          "#BAB86C", "#FF7538", "#FF2B2B", "#F8D568", "#E6A8D7", "#414A4C", "#FF6E4A", "#1CA9C9", "#FFCFAB", "#C5D0E6",
-          "#FDDDE6", "#158078", "#FC74FD", "#F78FA7", "#8E4585", "#7442C8", "#9D81BA", "#FE4EDA", "#FF496C", "#D68A59",
-          "#714B23", "#FF48D0", "#E3256B", "#EE204D", "#FF5349", "#C0448F", "#1FCECB", "#7851A9", "#FF9BAA", "#FC2847",
-          "#76FF7A", "#9FE2BF", "#A5694F", "#8A795D", "#45CEA2", "#FB7EFD", "#CDC5C2", "#80DAEB", "#ECEABE", "#FFCF48",
-          "#FD5E53", "#FAA76C", "#18A7B5", "#EBC7DF", "#FC89AC", "#DBD7D2", "#17806D", "#DEAA88", "#77DDE7", "#FFFF66",
-          "#926EAE", "#324AB2", "#F75394", "#FFA089", "#8F509D", "#FFFFFF", "#A2ADD0", "#FF43A4", "#FC6C85", "#CDA4DE",
-          "#FCE883", "#C5E384", "#FFAE42"]
-
-taxonomies = {'Asset-Type': {'url': 'https://www.us-api.morningstar.com/sal/sal-service/{type}/process/asset/v2/',
-                             'component': 'sal-components-mip-asset-allocation', 'jsonpath': '$.allocationMap',
-                             'category': '', 'percent': 'netAllocation', 'table': 0, 'column': 2,
-                             'map': {"AssetAllocNonUSEquity": "Stocks", "CANAssetAllocCanEquity": "Stocks",
-                                     "CANAssetAllocUSEquity": "Stocks", "CANAssetAllocInternationalEquity": "Stocks",
-                                     "AssetAllocUSEquity": "Stocks", "AssetAllocCash": "Cash",
-                                     "CANAssetAllocCash": "Stocks", "AssetAllocBond": "Bonds",
-                                     "CANAssetAllocFixedIncome": "Bonds", "UK bond": "Bonds",
-                                     "AssetAllocNotClassified": "Other", "AssetAllocOther": "Other",
-                                     "CANAssetAllocOther": "Other"}},
-              'Stock-style': {'url': 'https://www.us-api.morningstar.com/sal/sal-service/{type}/process/weighting/',
-                              'component': 'sal-components-mip-style-weight', 'jsonpath': '$', 'category': '',
-                              'percent': '', 'table': 9, 'column': 2,
-                              'map': {"largeBlend": "Large Blend", "largeGrowth": "Large Growth",
-                                      "largeValue": "Large Value", "middleBlend": "Mid-Cap Blend",
-                                      "middleGrowth": "Mid-Cap Growth", "middleValue": "Mid-Cap Value",
-                                      "smallBlend": "Small Blend", "smallGrowth": "Small Growth",
-                                      "smallValue": "Small Value", }},
-
-              'Sector': {'url': 'https://www.emea-api.morningstar.com/sal/sal-service/{type}/portfolio/v2/sector/',
-                         'component': 'sal-components-mip-sector-exposure', 'jsonpath': '$.EQUITY.fundPortfolio',
-                         'category': '', 'percent': '', 'table': 1, 'column': 0,
-                         'map': {"basicMaterials": "Basic Materials", "communicationServices": "Communication Services",
-                                 "consumerCyclical": "Consumer Cyclical", "consumerDefensive": "Consumer Defensive",
-                                 "energy": "Energy", "financialServices": "Financial Services",
-                                 "healthcare": "Healthcare", "industrials": "Industrials", "realEstate": "Real Estate",
-                                 "technology": "Technology", "utilities": "Utilities", }},
-              'Holding': {'url': 'https://www.emea-api.morningstar.com/sal/sal-service/{type}/portfolio/holding/v2/',
-                          'component': 'sal-components-mip-holdings', 'jsonpath': '$.equityHoldingPage.holdingList[*]',
-                          'category': 'securityName', 'percent': 'weighting', 'table': 6, 'column': 4, },
-              'Region': {'url': 'https://www.emea-api.morningstar.com/sal/sal-service/{type}/portfolio/regionalSector/',
-                         'component': 'sal-components-mip-region-exposure', 'jsonpath': '$.fundPortfolio',
-                         'category': '', 'percent': '', 'table': 2, 'column': 0,
-                         'map': {"northAmerica": "North America", "europeDeveloped": "Europe Developed",
-                                 "asiaDeveloped": "Asia Developed", "asiaEmerging": "Asia Emerging",
-                                 "australasia": "Australasia", "europeDeveloped": "Europe Developed",
-                                 "europeEmerging": "Europe Emerging / Russia", "japan": "Japan",
-                                 "latinAmerica": "Central & Latin America", "unitedKingdom": "United Kingdom",
-                                 "africaMiddleEast": "Middle East / Africa", },
-                         'map2': {"United States": "North America", "Canada": "North America",
-                                  "Western Europe - Euro": "Europe Developed",
-                                  "Western Europe - Non Euro": "Europe Developed",
-                                  "Emerging 4 Tigers": "Asia Developed", "Emerging Asia - Ex 4 Tigers": "Asia Emerging",
-                                  "Australasia": "Australasia", "Emerging Europe": "Europe Emerging / Russia",
-                                  "Japan": "Japan", "Central & Latin America": "Central & Latin America",
-                                  "United Kingdom": "United Kingdom", "Middle East / Africa": "Middle East / Africa",
-                                  "Not Classified": "Not Classified", }
-
-                         }, 'Country': {
-        'url': 'https://www.emea-api.morningstar.com/sal/sal-service/{type}/portfolio/regionalSectorIncludeCountries/',
-        'component': 'sal-components-mip-country-exposure', 'jsonpath': '$.fundPortfolio.countries[*]',
-        'category': 'name', 'percent': 'percent', 'table': 2, 'column': 0,
-        'map2': {"United States": "UnitedStates", "Canada": "Canada", "Western Europe - Euro": "Western Europe - Euro",
-                 "Western Europe - Non Euro": "Western Europe - Non Euro",
-                 "Emerging 4 Tigers": "Hong Kong, Singapore, SouthKorea and Taiwan",
-                 "Emerging Asia - Ex 4 Tigers": "Asia Emerging", "Australasia": "Australasia",
-                 "Emerging Europe": "Europe Emerging / Russia", "Japan": "Japan",
-                 "Central & Latin America": "Central & Latin America", "United Kingdom": "United Kingdom",
-                 "Middle East / Africa": "Middle East / Africa", "Not Classified": "Not Classified", }
-
-    }, }
 
 
 class Isin2secid:
